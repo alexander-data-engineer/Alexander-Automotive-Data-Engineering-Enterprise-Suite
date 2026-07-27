9173e1505f70ddbe5de8ba0967b4e13946e7e7cb
alexander-data-engineer <alexander.automotive.eng@gmail.com>
2026-07-27 06:46:25 -0300

    Add files via upload

--- aula_002h_inbound_gateway.py
+++ aula_002h_inbound_gateway.py
@@ -1,8 +1,23 @@
---- BMW MUNICH INDUSTRIAL REPOSITORY ---
-[BRAND] -> Montadora Aduaneira: -> BMW MUNICH FACTORY
-[PAYLOAD] -> Chassi Purificado: -> [VIN-DE-DE93450-DE_CARGO_LATCH_SECURE]
-[CONTRACT] -> Lacre de Monopolio: -> B2B-2026-ALEXANDER-ELITE
-======================================== | PROCESS_COMPLETE
+def inbound_parts_gateway():
+    
+    bmwTargetBrand = "BMW MUNICH FACTORY"
+    un_if_i_edContractId = "B2B-2026-ALEXANDER-ELITE"
+    
+    
+    
+    rawCargoPayload = "    [VIN-DE-DE93450-DE_CARGO_LATCH_SECURE]    "
+    
+    
+    purifiedTxtBuffer = rawCargoPayload.strip()
+    
+    
+    print("--- BMW MUNICH INDUSTRIAL REPOSITORY ---", sep="-")
+    print("[BRAND]", "Montadora Aduaneira:", bmwTargetBrand, sep=" -> ")
+    print("[PAYLOAD]", "Chassi Purificado:", purifiedTxtBuffer, sep=" -> ")
+    print("[CONTRACT]", "Lacre de Monopolio:", un_if_i_edContractId, sep=" -> ")
+    print("========================================", "PROCESS_COMPLETE", sep=" | ", end="\n")
 
 
-** Process exited - Return Code: 0 **
+inbound_parts_gateway()
+
+
